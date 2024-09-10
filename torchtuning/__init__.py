829f538d54c08b4051bacab538ff9a3943a5b0e0
saibie1677 <saibie1677@gmail.com>
2024-09-10 05:02:23 +0000

    inst 모델 작동 안 해서 torchtune이 제공하는 save_checkpoint로 다시 저장 후 작동 테스트

--- torchtuning/__init__.py
+++ torchtuning/__init__.py
@@ -1,7 +1,9 @@
 from ._text_datasets import koalpaca_dataset, kowiki_dataset, text_completion_dataset
+from ._chat_datasets import custom_chat_dataset
 
 __all__ = [
     'koalpaca_dataset',
     'kowiki_dataset',
-    'text_completion_dataset'
+    'text_completion_dataset',
+    'custom_chat_dataset',
 ]
