2bb896cb0a3068e270a12225e65cde2bae057e86
saibie1677 <saibie1677@gmail.com>
2024-09-11 09:05:38 +0000

    Instruction Model 위한 데이터셋 및 config 수정

--- torchtuning/_chat_datasets.py
+++ torchtuning/_chat_datasets.py
@@ -1,21 +1,39 @@
 from typing import Any, Dict, List, Mapping, Optional
 from torchtune.modules.tokenizers import Tokenizer
-from torchtune.data import Message
+from torchtune.data import Message, ChatFormat
 from torchtune.datasets import ChatDataset
 
-def _converter(sample: Mapping[str, Any]) -> List[Message]:
-    system_msg = sample['system']
-    text_msgs = [m.strip() for m in sample['conversations'].split('[sep]')]
+class CustomChatFormat(ChatFormat):
+    system = ""
+    user = ""
+    assistant = ""
+
+    @classmethod
+    def format(
+        cls,
+        sample: List[Message],
+    ) -> List[Message]:
+        return sample
+
+chat_format = CustomChatFormat()
+
+def _converter(sample: Mapping[str, Any], train_on_input: bool=False) -> List[Message]:
+    system_msg = sample.get('system', '')
+    text_msgs = [sample['user'], sample['assistant']]
     
     is_user = True
     # Multi-turn dataset
-    msgs = [
-        Message(
-            role="system",
-            content=system_msg,
-            masked=True, # Mask if not training on prompt
-        )
-    ]
+    if system_msg:
+        msgs = [
+            Message(
+                role="system",
+                content=system_msg,
+                masked=True, # Mask if not training on prompt
+            )
+        ]
+    else:
+        msgs = []
+        
     for m in text_msgs:
         msgs.append(
             Message(
@@ -40,7 +58,7 @@ def custom_chat_dataset(
         source=source,
         convert_to_messages=_converter,
         # Llama3 does not need a chat format
-        chat_format=None,
+        chat_format=chat_format,
         max_seq_len=max_seq_len,
         **load_dataset_kwargs,
     )
